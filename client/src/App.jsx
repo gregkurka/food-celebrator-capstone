b3f702e3ba7f1400301d64117e9d0855c62c8737
EricRivera-Snow <eric.rivera@aya.yale.edu>
2025-02-19 16:14:08 -0500

    created app routes and installed various packages: '

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Account from "./pages/Account";
+import Feed from "./pages/Feed";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+// import "./App.css";
 
 function App() {
-  return <></>;
+  <div>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/feed" element={<Feed />} />
+    </Routes>
+    <Footer />
+  </div>;
 }
 
 export default App;
