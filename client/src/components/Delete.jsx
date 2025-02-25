81ba0f1a7cf6e032b7c29da7d3b6be22a8db96b0
Bikna <hbikna@gmail.com>
2025-02-25 08:42:26 -0800

    finished delete

---
+++ client/src/components/Delete.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const URL = "http://localhost:3000/api";
+
+function Delete({ userId, postId, onDelete }) {
+  //   console.log(`Deleting post ${postId} for user ${userId}`);
+
+  const handleDelete = async (userId, postId) => {
+    console.log(postId);
+    try {
+      const response = await axios.delete(
+        `${URL}/users/${userId}/pictures/${postId}`
+      );
+      const data = response.json;
+      if (response.status === 200) {
+        console.log("Post deleted successfully");
+        onDelete(postId);
+      }
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleDelete(userId, postId)} data-id={userId}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default Delete;
