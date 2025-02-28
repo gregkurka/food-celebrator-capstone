0edfa86dd329a931518cf610ace97ba249173eb2
Michael Woodard <mr.m.woodard@gmail.com>
2025-02-28 16:18:21 -0500

    add video with sound

--- client/src/components/VideoDisplayer.jsx
+++ client/src/components/VideoDisplayer.jsx
@@ -4,7 +4,7 @@ function VideoDisplayer() {
   return (
     <div className="flex justify-center items-center w-full max-w-3xl p-4 bg-black rounded-lg shadow-lg">
       <video controls autoPlay loop muted className="w-full h-auto rounded-lg">
-        <source src="/IntroVideoNoSound.mp4" type="video/mp4" />
+        <source src="/FoodCelebrator.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
